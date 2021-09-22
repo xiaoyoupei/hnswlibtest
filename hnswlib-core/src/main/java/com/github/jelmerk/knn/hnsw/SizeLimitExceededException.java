931dbb5acefc574f1c74bc747c4b42015878a3e8
xiaoyoupei <masu_cs_xyp@163.com>
2021-09-22 15:47:06 +0800

    hnswlib 自定义key和Item的java序列化方式换成kryo序列化

--- hnswlib-core/src/main/java/com/github/jelmerk/knn/hnsw/SizeLimitExceededException.java
+++ hnswlib-core/src/main/java/com/github/jelmerk/knn/hnsw/SizeLimitExceededException.java
@@ -3,11 +3,13 @@ package com.github.jelmerk.knn.hnsw;
 import com.github.jelmerk.knn.IndexException;
 
 /**
+ * 抛出，以指示已超过索引的大小。
  * Thrown to indicate the size of the index has been exceeded.
  */
 public class SizeLimitExceededException extends IndexException {
 
     /**
+     * 使用指定的详细消息构造一个SizeLimitExceededException。
      * Constructs a SizeLimitExceededException with the specified detail message.
      *
      * @param message the detail message.
