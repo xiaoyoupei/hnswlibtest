d9053b1be384ecb66b7225f2b3db3aac430882d3
xiaoyoupei <masu_cs_xyp@163.com>
2021-09-22 12:24:39 +0800

    hnswlib 自定义key和Item的java序列化方式换成kryo序列化

--- hnswlib-core/src/main/java/com/github/jelmerk/knn/hnsw/KryoDataItem.java
+++ hnswlib-core/src/main/java/com/github/jelmerk/knn/hnsw/KryoDataItem.java
@@ -16,38 +16,38 @@ public class KryoDataItem implements ObjectSerializer<DataItem> {
 
     private static final long serialVersionUID = 1L;
 
-    /**
-     * ThreadLocal牺牲空间来换取并发线程安全
-     */
-    private static final ThreadLocal<Kryo> kryos = new ThreadLocal<Kryo>() {
-        protected Kryo initialValue() {
-            Kryo kryo = new Kryo();
-
-            kryo.setReferences(true); ////支持循环引用，默认值就是 true
-
-            kryo.setRegistrationRequired(false); //关闭注册行为，默认值就是 false
-
-            //Fix the NPE bug when deserializing Collections.
-            ((Kryo.DefaultInstantiatorStrategy) kryo.getInstantiatorStrategy()).setFallbackInstantiatorStrategy(new StdInstantiatorStrategy());
-            kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());
-            return kryo;
-        }
-    };
-
-
-//    static Kryo kryo = new Kryo();
+//    /**
+//     * ThreadLocal牺牲空间来换取并发线程安全
+//     */
+//    private static final ThreadLocal<Kryo> kryos = new ThreadLocal<Kryo>() {
+//        protected Kryo initialValue() {
+//            Kryo kryo = new Kryo();
 //
-//    static {
-//        kryo.setReferences(true); //默认值就是 true，添加此行的目的是为了提醒维护者，不要改变这个配置
+//            kryo.setReferences(true); ////支持循环引用，默认值就是 true
 //
-//        //不强制要求注册类（注册行为无法保证多个 JVM 内同一个类的注册编号相同；而且业务系统中大量的 Class 也难以一一注册）
-//        kryo.setRegistrationRequired(false); //默认值就是 false，添加此行的目的是为了提醒维护者，不要改变这个配置
+//            kryo.setRegistrationRequired(false); //关闭注册行为，默认值就是 false
 //
-//        //Fix the NPE bug when deserializing Collections.
-//        ((Kryo.DefaultInstantiatorStrategy) kryo.getInstantiatorStrategy())
-//                .setFallbackInstantiatorStrategy(new StdInstantiatorStrategy());
-//        kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());
-//    }
+//            //Fix the NPE bug when deserializing Collections.
+//            ((Kryo.DefaultInstantiatorStrategy) kryo.getInstantiatorStrategy()).setFallbackInstantiatorStrategy(new StdInstantiatorStrategy());
+//            kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());
+//            return kryo;
+//        }
+//    };
+
+
+    static Kryo kryo = new Kryo();
+
+    static {
+        kryo.setReferences(true); //默认值就是 true，添加此行的目的是为了提醒维护者，不要改变这个配置
+
+        //不强制要求注册类（注册行为无法保证多个 JVM 内同一个类的注册编号相同；而且业务系统中大量的 Class 也难以一一注册）
+        kryo.setRegistrationRequired(false); //默认值就是 false，添加此行的目的是为了提醒维护者，不要改变这个配置
+
+        //Fix the NPE bug when deserializing Collections.
+        ((Kryo.DefaultInstantiatorStrategy) kryo.getInstantiatorStrategy())
+                .setFallbackInstantiatorStrategy(new StdInstantiatorStrategy());
+        kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());
+    }
 
     /**
      * kryo写序列化
@@ -57,7 +57,7 @@ public class KryoDataItem implements ObjectSerializer<DataItem> {
      * @throws IOException
      */
     public void write(DataItem item, ObjectOutput out) throws IOException {
-        Kryo kryo = kryos.get();
+        //Kryo kryo = kryos.get();
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(new OutputStream() {
             @Override
             public void write(int b) throws IOException {
@@ -77,7 +77,7 @@ public class KryoDataItem implements ObjectSerializer<DataItem> {
      * @throws IOException
      */
     public DataItem read(ObjectInput in) throws IOException {
-        Kryo kryo = kryos.get();
+        //Kryo kryo = kryos.get();
         ObjectInputStream objectInputStream = new ObjectInputStream(new InputStream() {
             @Override
             public int read() throws IOException {
